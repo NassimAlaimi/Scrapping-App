d277259ec5e882023f02887adcae116a4f87e7bb
NassimAlaimi <nassim.alaimi@epitech.eu>
2023-04-12 00:56:42 +0200

    add route sys with page not found

--- Get-Logo/client/src/App.js
+++ Get-Logo/client/src/App.js
@@ -1,43 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+
+// function that display Page Not Found if the page isn't found
+const PageNotFound = () => {
+  return (
+    <h1>404: Page Not Found</h1>
+  )
+}
 
 function App() {
-  const [url, setUrl] = useState('');
-  const [response, setResponse] = useState('');
-  const [logo, setLogo] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ url })
-    };
-
-    try {
-      const response = await fetch('http://localhost:4000/', requestOptions);
-      const data = await response.json();
-      setResponse(data.message);
-      setLogo(data.logo);
-    } catch (error) {
-      console.error(error);
-      setResponse('An error occurred');
-    }
-  };
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          URL:
-          <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      <br/>
-      {response && <div>Server response: {response}</div>}
-      {logo && <img src={logo} alt="Logo" />}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Get-Logo/client/src/pages/home.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const Home = () => {
+
+  const [url, setUrl] = useState('');
+  const [response, setResponse] = useState('');
+  const [logo, setLogo] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url })
+    };
+
+    try {
+      const response = await fetch('http://localhost:4000/', requestOptions);
+      const data = await response.json();
+      setResponse(data.message);
+      setLogo(data.logo);
+    } catch (error) {
+      console.error(error);
+      setResponse('An error occurred');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          URL:
+          <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      {response && <div>Server response: {response}</div>}
+      {logo && <img src={logo} alt="Logo" />}
+    </div>
+  )
+}
+
+export default Home
