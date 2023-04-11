4a314cc38804e51a75a606d4e35311ca465c623a
NassimAlaimi <nassim.alaimi@epitech.eu>
2023-04-11 16:01:13 +0200

    server use cheerio because metascrapper isn't working

--- my-app-docker/api/app.js
+++ my-app-docker/api/app.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const cors = require('cors');
-const port = 4000;
+const bodyParser = require('body-parser');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
 const app = express();
+const port = 4000;
+
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-app.post('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+app.post('/', async (req, res) => {
+  const { url } = req.body;
+  if (!url) {
+    return res.status(400).json({ error: 'Missing URL parameter' });
+  }
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const logo = $('head link[rel="icon"]').attr('href') || $('head link[rel="shortcut icon"]').attr('href');
+    console.log('logo:', logo);
+    res.json({ logo });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
